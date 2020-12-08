be780abce91e468d570ad31c5c71bfbc443b0e46
Mridul <mhayaran123@gmail.com>
2020-12-08 20:48:26 +0530

    working text highlight

--- main.rs
+++ main.rs
@@ -1,15 +1,26 @@
-use cursive::event::{Event, Key};
-use cursive::views::{Dialog, TextView, LinearLayout, EditView, OnEventView, TextArea};
+use cursive::event::{Key};
+use cursive::align::HAlign;
+use cursive::views::{Dialog, EditView, ResizedView, TextView, LinearLayout, ProgressBar};
 use cursive::traits::*;
 use cursive::Cursive;
 use cursive::menu::MenuTree;
-// use cursive::theme::{BaseColor, Color, Effect, Style, ColorStyle};
-// use cursive::utils::markup::StyledString;
+use cursive::views::PaddedView;
+
+use std::sync::Once;
+
+use cursive::{
+    theme::{Effect, Style},
+    utils::markup::StyledString,
+    views,
+};
 
 // This example define a custom view that prints any event it receives.
 // This is a handy way to check the input received by cursive.
 //add a
 
+// content: vec<&str> = "just as I'm giving up, my heart is palpitating. Here comes another one, it's so intoxicating. And being where I've been, I know that in the end I'll do it all again.".split(" ").collect();
+
+#[warn(unused_imports)]
 fn main() {
     let mut siv = cursive::default();
 
@@ -62,49 +73,81 @@ fn racing_screen(s: &mut Cursive, name: &str)
     {
         s.add_layer(Dialog::info("You forgot gib name"));
     } else {
-        //todo fetch content from api or database
-        let content = "just as I'm giving up, my heart is palpitating. Here comes another one, it's so intoxicating. And being where I've been, I know that in the end I'll do it all again.";
-        // let alignment = VAlign.get_offset();
-
-        let racing_text = Dialog::around(
-                        TextView::new(content)
-                        .with_name("edittext")
-                        .fixed_height(10),
-        );
-
-        let input_text = Dialog::around(
-                        TextArea::new()
-                        .with_name("text")
-                        .fixed_height(5),
-        );
-    
-        let second_racing_text = Dialog::around(
-            TextView::new(name),
-        );
-    
-
-        // s.set_autorefresh(false);
         s.pop_layer();
 
-        // let mut count = 0;
-        s.add_layer( OnEventView::new(Dialog::around(LinearLayout::vertical()
-                .child(racing_text)
-                // .child(input_text)
-                .child(second_racing_text)
-        // )).on_event(Event::Char(content.chars().nth(0).unwrap()), move |s| racing(s)));
-           )).on_event(Key::Enter, |s| racing(s)));
+        let type_text = "Isn't that cool";
 
+        let text : Vec<&str> = type_text.split_whitespace().collect();
+        let mut styled = StyledString::plain("");
+        
+        for i in text {
+            styled.append(StyledString::plain(i));
+            styled.append(" ");
+        }
+
+        let racing_text = ResizedView::with_max_width(80, 
+            Dialog::new()
+            .content(
+
+                TextView::new(styled).with_name("1"),
+            )
+            .h_align(HAlign::Left)
+        );
+
+        let typing_field = ResizedView::with_max_width(80, EditView::new().on_edit(on_edit));
+
+
+        let userinfo = ResizedView::with_max_width(80, 
+            Dialog::new()
+            .content(
+                LinearLayout::horizontal()
+                    .child( PaddedView::lrtb(0, 3, 0 , 0 , TextView::new(name)) )
+                    .child(
+                        ProgressBar::new()
+                                .with_task(|counter| {
+                                    for _ in 0..100 {
+                                        counter.tick(1);
+                                    }
+                                }),
+                    ),
+            )
+            .h_align(HAlign::Left)
+        );
+        s.set_autorefresh(false);
+        s.add_layer(
+            LinearLayout::vertical()
+            .child(racing_text)
+            .child(typing_field)
+            .child(userinfo)
+        );
     }
 }
 
-fn racing(siv: &mut Cursive) {
-    // let mut count : usize = 0;
-    siv.screen_mut().call_on_name("edittext", |view: &mut TextView|{
-        let content = text_color(view.get_content().source());
-        view.set_content(content);
+static START: Once = Once::new();
+
+fn on_edit(s: &mut cursive::Cursive, edit_text: &str, _cursor: usize) {
+
+    START.call_once(||{
+        s.add_layer(Dialog::info("created by a anon pajeet"));
+    });
+
+    s.call_on_name("1", |t: &mut views::TextView| {
+    
+        let mut new_content = StyledString::new();
+        for span in t.get_content().spans() {
+            let new_style = if edit_text == span.content {
+                Style::from(Effect::Bold)
+            } else {
+                // `span.attr` here is the `&Style` applied to the span
+                *span.attr
+            };
+            // `span.content` here is the visible text for this span.   
+            new_content.append_styled(span.content, new_style);
+        }
+        t.set_content(new_content);
     });
 }
 
-fn text_color(text: &str) -> String {
-    text.chars().rev().collect()
-}
+
+// when mouseclick on the edit text start the race
+// when the value in race matches the value with the respective indexed content color the content and clear the edit text
